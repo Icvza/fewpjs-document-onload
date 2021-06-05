15f6c73b8bee487b0e87cac2edc2ed8606258060
Isaac <devisaacalejandro@gmail.com>
2021-06-05 00:52:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", whatATimeTobe)
+
+function whatATimeTobe() {
+    document.getElementById('text').innerHTML = "This is really cool!"
+}
